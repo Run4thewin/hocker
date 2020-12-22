a2440901da6a7e90862415b5637bc0eb66aeda26
Brendaluizapereira <brendaluizacpereira@gmail.com>
2020-12-22 12:33:56 -0300

    Telas do protótipo ok

--- src/app/views/register-form/register-form.component.ts
+++ src/app/views/register-form/register-form.component.ts
@@ -22,16 +22,15 @@ export class RegisterFormComponent implements OnInit {
     this.contactForm = this.formBuilder.group ({
       nombre: [form.nombre, [Validators.required]],
       email: [form.email, [Validators.email]],
-      telefono: [form.telefono, [Validators.required]],
+      //telefono: [form.telefono, [Validators.required]],
       mensaje: [form.mensaje]
     }) 
   }
 
   onSubmit() {
-    // aqui você pode implementar a logica para fazer seu formulário salvar
+
     console.log(this.contactForm.value);
 
-    // Usar o método reset para limpar os controles na tela
     this.contactForm.reset(new Form());
   }
 
