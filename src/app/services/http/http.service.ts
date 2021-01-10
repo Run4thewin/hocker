10e52dfe79a0f7103af7e2fa7540d1f24d5affc6
Brendaluizapereira <brendaluizacpereira@gmail.com>
2021-01-09 21:15:33 -0300

    Adicionando confirmação de envio

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,20 +7,15 @@ import { Observable } from 'rxjs';
 })
 export class HttpService {
 
-  url = "";
-
-  //Remove this ! later
-
-  options!: {
-    headers?: HttpHeaders | {[header: string]: string | string[]},
-  }
-
   constructor(private http : HttpClient) { }
 
-  addForm(form: any) : Observable<any>{
-    
-    console.log (form);
-    return this.http.post(this.url, form, this.options)
+  options!: {
+    headers?: HttpHeaders | { [header: string]: string | string[]; };
+  };
 
+  apiURL = "";
+
+  addForm(form : any) : Observable<any>{
+    return this.http.post(this.apiURL, form, this.options);
   }
 }
