a2306bbaceb8f3f8481bfd784c601586cc7e184d
Brendaluizapereira <brendaluizacpereira@gmail.com>
2021-02-03 11:38:53 -0300

    Alterações finais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpService } from './services/http/http.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'SitioHocker';
+
+  //closeCookiesAlert calls okCookies, that sets an item CookiesSeen
+  //as "accepted" in Local Storage. Another function, acceptedCookies, returns boolean
+  //value for local storage item CookiesSeen. ShowCookiesAlert will always check for it
+  //and return the opposite. So, if cookies were accepted, showCookiesAlert will be negative.
+
+  showCookiesAlert = true;
+
+  constructor(private trackCookies : HttpService) { }
+
+  //Deciding whether to display cookies alert
+  
+  //Saving acceptance of cookies
+  closeCookiesAlert() {
+    this.trackCookies.okCookies();
+    this.showCookiesAlert = false;
+  }
 
 }
